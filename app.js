af8804391d492de35fd0c8b01a331c693852826a
aravindkeelapatta <aravindkeelapatta1994@gmail.com>
2017-05-18 11:15:08 +0530

    adds routing updated files

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-var app = angular.module("myApp", []);
-
-
-app.controller("myController", ["$scope", function($scope) {
-
-        $scope.message = "Hello World";
-	}]);
+var app = angular.module("myApp", ["ngRoute"])
+                 .config(["$locationProvider","$routeProvider", function($locationProvider,$routeProvider) {
+                 	$locationProvider.hashPrefix('')
+               
+			       $routeProvider
+				    .when("/", {
+					   templateUrl: "pages/index.html",
+					   controller : "indexController"
+					})
+					.when("/home", {
+						templateUrl : "pages/home.html",
+						controller  : "homeController"
+					})
+					.when("/register", {
+					   templateUrl: "pages/register.html",
+					   controller : "registerController"
+					})
+					.when("/login", {
+					   templateUrl: "pages/login.html",
+					   controller : "loginController"
+					})
+				 }])
+				 .controller("indexController", function($scope) {
+					 $scope.message = "Index Page"
+				 })
+				 .controller("homeController", function($scope) {
+					 $scope.message = "Home Page"
+				 })
+				 .controller("registerController", function($scope) {
+					 $scope.message = "Register Page"
+				 })
+				 .controller("loginController", function($scope) {
+					 $scope.message = "Login Page"
+				 })
